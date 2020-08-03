d8427d2da3498b57ab9a855e79516c16e6765e99
Alexis Thinardon <alexis.thinardon@gmail.com>
2020-08-03 09:13:42 +0200

    Add first unit test with circle ci

---
+++ application/backend/tests/FirstTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class FirstTest  extends TestCase
+{
+    public function testFirst()
+    {
+        $this->assertTrue(true);
+    }
+}
