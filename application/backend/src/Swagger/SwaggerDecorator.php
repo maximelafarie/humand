343559dee9688543b42109bde34498ca7d6680f6
Alexis Thinardon <alexis.thinardon@gmail.com>
2020-08-06 09:09:36 +0200

    Add token path in api documentation

---
+++ application/backend/src/Swagger/SwaggerDecorator.php
@@ -0,0 +1,77 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Swagger;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+final class SwaggerDecorator implements NormalizerInterface
+{
+    private NormalizerInterface $decorated;
+
+    public function __construct(NormalizerInterface $decorated)
+    {
+        $this->decorated = $decorated;
+    }
+
+    public function supportsNormalization($data, string $format = null): bool
+    {
+        return $this->decorated->supportsNormalization($data, $format);
+    }
+
+    public function normalize($object, string $format = null, array $context = [])
+    {
+        $docs = $this->decorated->normalize($object, $format, $context);
+
+        $docs['components']['schemas']['Credentials'] = [
+            'type' => 'object',
+            'properties' => [
+                'username' => [
+                    'type' => 'string',
+                    'example' => 'api',
+                ],
+                'password' => [
+                    'type' => 'string',
+                    'example' => 'api',
+                ],
+            ],
+        ];
+
+        $tokenDocumentation = [
+            'paths' => [
+                '/api/token' => [
+                    'get' => [
+                        'tags' => ['User'],
+                        'operationId' => 'postCredentialsItem',
+                        'summary' => 'Get JWT token to login.',
+                        'requestBody' => [
+                            'description' => 'Create new JWT Token',
+                            'content' => [
+                                'application/json' => [
+                                    'schema' => [
+                                        '$ref' => '#/components/schemas/Credentials',
+                                    ],
+                                ],
+                            ],
+                        ],
+                        'responses' => [
+                            Response::HTTP_OK => [
+                                'description' => 'Get JWT token',
+                                'content' => [
+                                    'application/json' => [
+                                        'schema' => [
+                                            '$ref' => '#/components/schemas/Token',
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+
+        return array_merge_recursive($docs, $tokenDocumentation);
+    }
+}
---
+++ application/backend/tests/Swagger/SwaggerDecoratorTest.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Tests\Swagger;
+
+use App\Swagger\SwaggerDecorator;
+use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
+use Prophecy\Prophecy\ObjectProphecy;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class SwaggerDecoratorTest extends TestCase
+{
+    /**
+     * @var NormalizerInterface|ObjectProphecy
+     */
+    private $normalizer;
+
+    /**
+     * @var SwaggerDecorator
+     */
+    private $swagger;
+
+    public function setUp()
+    {
+        $this->normalizer = $this->prophesize(NormalizerInterface::class);
+
+        $this->swagger = new SwaggerDecorator($this->normalizer->reveal());
+    }
+
+    public function testItIsAnNormalizerInteraface()
+    {
+        $this->assertInstanceOf(NormalizerInterface::class, $this->swagger);
+    }
+
+    public function testItNormalizeNewTokenPath()
+    {
+        $this->normalizer->normalize(Argument::cetera())->shouldBeCalled()->willReturn([]);
+
+        $result = $this->swagger->normalize(Argument::cetera());
+
+        $this->assertArrayHasKey('paths', $result);
+        $this->assertArrayHasKey('/api/token', $result['paths']);
+    }
+}
